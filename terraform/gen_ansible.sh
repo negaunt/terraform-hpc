c718eb0d40a2fd80416da07af5df41e59e8786f5
Nate Gauntt <negaunt@gmail.com>
2026-09-23 16:17:54 -0700

    fix json output selector

--- terraform/gen_ansible.sh
+++ terraform/gen_ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Begin gen_ansible.sh - generate terraform artifacts needed by ansible later
 ANS_OUT="../ansible/tf_output.json";
-terraform output > "$ANS_OUT" 
+terraform output -json > "$ANS_OUT" 
 echo "Wrote terraform output: $ANS_OUT"
